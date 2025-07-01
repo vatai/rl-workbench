3b4f93051cfbfecf84b5e04dac2922a80dba70c2
Emil VATAI <emil.vatai@gmail.com>
2025-07-01 17:34:01 +0900

    Update greedy.py

--- chapter2/greedy.py
+++ chapter2/greedy.py
@@ -3,17 +3,20 @@ import numpy as np
 
 
 class Environment:
-    def __init__(self, n):
-        self.mus = np.random.normal(size=[n])
+    def __init__(self, k):
+        self.mus = np.random.normal(size=[k])
 
     def action(self, a):
         mu = self.mus[a]
         return np.random.normal(loc=mu)
 
+    def num_arms(self):
+        return len(self.mus)
+
 
 def create_violinplot(env, reps=500):
     all_data = []
-    n = len(env.mus)
+    n = env.num_arms()
     for a in range(n):
         data = [env.action(a) for _ in range(reps)]
         all_data.append(data)
@@ -23,9 +26,23 @@ def create_violinplot(env, reps=500):
 
 
 def main():
-    n = 10
-    env = Environment(n)
-    create_violinplot(env)
+    env = Environment(10)
+    # create_violinplot(env)
+
+    k = env.num_arms()
+    estimations = np.array([-np.inf for _ in range(k)])
+    counts = np.zeros(k)
+
+    steps = 1000
+    epsilon = 0.1
+    for _ in range(steps):
+        if np.random.uniform() < epsilon:
+            a = np.random.random_integers(0, 9)
+        else:
+            a = np.argmax(estimations / counts)
+        r = env.action(a)
+        counts[a] += 1
+        estimations[a] += r
 
 
 main()
